9418df469e0a45c029382f87cbba53b018b2890d
PandeyPankaj1234 <Pankajpandey9351@gmail.com>
2025-12-18 11:56:03 +0530

    ReverseInteger  Program commit

---
+++ core-java-practice/leet-code-codebase/ReverseInteger.java
@@ -0,0 +1,12 @@
+class ReverseInteger {
+    public static int reverse(int x) {
+        int rev = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            rev = rev * 10 + digit;
+            x /= 10;
+        }
+        return rev;
+    }
+}
