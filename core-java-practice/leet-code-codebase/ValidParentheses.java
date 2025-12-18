cc54988e2a66644255c1a99e1cdd355447124ed8
PandeyPankaj1234 <Pankajpandey9351@gmail.com>
2025-12-18 12:01:53 +0530

    ValidParenthesis  Program commit

---
+++ core-java-practice/leet-code-codebase/ValidParentheses.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+class ValidParentheses {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[')
+                stack.push(ch);
+            else {
+                if (stack.isEmpty())
+                    return false;
+                char open = stack.pop();
+                if ((ch == ')' && open != '(') ||
+                        (ch == '}' && open != '{') ||
+                        (ch == ']' && open != '['))
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
