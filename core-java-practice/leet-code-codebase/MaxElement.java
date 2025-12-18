269c35eebff73656639f9c937f0ff0503ff22c13
PandeyPankaj1234 <Pankajpandey9351@gmail.com>
2025-12-18 12:02:55 +0530

    MaxElement  Program commit

---
+++ core-java-practice/leet-code-codebase/MaxElement.java
@@ -0,0 +1,10 @@
+class MaxElement {
+    public static int findMax(int[] arr) {
+        int max = arr[0];
+        for (int num : arr) {
+            if (num > max)
+                max = num;
+        }
+        return max;
+    }
+}
