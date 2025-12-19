22d59ccb4397fd6f464afff90009f670b358edfc
PandeyPankaj1234 <Pankajpandey9351@gmail.com>
2025-12-19 15:38:01 +0530

    [Pankaj Pandey] Added SpringSeason Program

---
+++ core-java-practice/gcr-codebase/control-flow/SpringSeason.java
@@ -0,0 +1,8 @@
+class SpringSeason {
+    public static void main(String[] args) {
+        int month = Integer.parseInt(args[0]);
+        int day = Integer.parseInt(args[1]);
+        boolean isSpringSeason = (month == 3 && day >= 20) || (month == 4) || (month == 5) || (month == 6 && day <= 20);
+        System.out.println(isSpringSeason ? "Its a Spring Season" : "Not a Spring Season");
+    }
+}
