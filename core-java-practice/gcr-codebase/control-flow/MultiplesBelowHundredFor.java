dfae3a0cdb98363c51c522d789eb64e0b23aefe9
PandeyPankaj1234 <Pankajpandey9351@gmail.com>
2025-12-19 16:58:33 +0530

    [Pankaj Pandey] Add. Added MultiplesBelowHundredFor Program

---
+++ core-java-practice/gcr-codebase/control-flow/MultiplesBelowHundredFor.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class MultiplesBelowHundredFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        if (number > 0 && number < 100) {
+            for (int value = 100; value >= 1; value--)
+                if (value % number == 0)
+                    System.out.println(value);
+        }
+        sc.close();
+    }
+}
