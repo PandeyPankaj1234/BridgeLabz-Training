defa5b4df0d49bb86b5252bee77bb191bdb402a4
PandeyPankaj1234 <Pankajpandey9351@gmail.com>
2025-12-19 16:55:16 +0530

    [Pankaj Pandey] Add. Added FizzBuzzWhile Program

---
+++ core-java-practice/gcr-codebase/control-flow/FizzBuzzWhile.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class FizzBuzzWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        if (number > 0) {
+            int counter = 1;
+            while (counter <= number) {
+                if (counter % 3 == 0 && counter % 5 == 0)
+                    System.out.println("FizzBuzz");
+                else if (counter % 3 == 0)
+                    System.out.println("Fizz");
+                else if (counter % 5 == 0)
+                    System.out.println("Buzz");
+                else
+                    System.out.println(counter);
+                counter++;
+            }
+        }
+        sc.close();
+    }
+}
