1520ebbab68ad16e1a3b9db9f5bb090bd42209a0
PandeyPankaj1234 <Pankajpandey9351@gmail.com>
2025-12-19 21:33:19 +0530

    [Pankaj Pandey] Add. Added DayOfWeek Program

---
+++ core-java-practice/gcr-codebase/control-flow/DayOfWeek.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class DayOfWeek {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int month = sc.nextInt();
+        int day = sc.nextInt();
+        int year = sc.nextInt();
+        int y = year - (14 - month) / 12;
+        int x = y + y / 4 - y / 100 + y / 400;
+        int m = month + 12 * ((14 - month) / 12) - 2;
+        int dayOfWeek = (day + x + (31 * m) / 12) % 7;
+        System.out.println(dayOfWeek);
+        sc.close();
+    }
+}
