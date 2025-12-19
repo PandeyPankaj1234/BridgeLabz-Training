a1d67ce42e248d8611777cba3467870edc9a83ad
PandeyPankaj1234 <Pankajpandey9351@gmail.com>
2025-12-19 15:47:20 +0530

    [Pankaj Pandey] Add. Added SumUntilZero Program

---
+++ core-java-practice/gcr-codebase/control-flow/SumUntilZero.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class SumUntilZero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double totalSum = 0;
+        while (true) {
+            double inputValue = sc.nextDouble();
+            if (inputValue == 0)
+                break;
+            totalSum += inputValue;
+        }
+        System.out.println(totalSum);
+        sc.close();
+    }
+}
