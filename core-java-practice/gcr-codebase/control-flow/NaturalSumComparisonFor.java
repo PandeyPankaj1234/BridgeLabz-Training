cf82f32b8a8e1b841c458c273041f4c45e7ec304
PandeyPankaj1234 <Pankajpandey9351@gmail.com>
2025-12-19 15:50:51 +0530

    [Pankaj Pandey] Add. Added NaturalSumComparisonFor Program

---
+++ core-java-practice/gcr-codebase/control-flow/NaturalSumComparisonFor.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class NaturalSumComparisonFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        if (number > 0) {
+            int loopSum = 0;
+            for (int counter = 1; counter <= number; counter++)
+                loopSum += counter;
+            int formulaSum = number * (number + 1) / 2;
+            System.out.println(loopSum + " " + formulaSum);
+        }
+        sc.close();
+    }
+}
