564dfcd9d763114b65a4c622ceb1f7e265022178
PandeyPankaj1234 <Pankajpandey9351@gmail.com>
2025-12-19 16:51:13 +0530

    [Pankaj Pandey] Add. Added MultiplicationTableSixToNine Program

---
+++ core-java-practice/gcr-codebase/control-flow/MultiplicationTableSixToNine.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class MultiplicationTableSixToNine {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        for (int multiplier = 6; multiplier <= 9; multiplier++)
+            System.out.println(number + " * " + multiplier + " = " + (number * multiplier));
+        sc.close();
+    }
+}
