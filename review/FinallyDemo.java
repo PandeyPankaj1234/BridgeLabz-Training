53577c3aa7b6d7e36ca19b308e092dbf2226b00b
PandeyPankaj1234 <Pankajpandey9351@gmail.com>
2026-01-05 12:56:09 +0530

    [Pankaj Pandey] Add.Refactor scenario based review on review branch

--- review/FinallyDemo.java
+++ review/FinallyDemo.java
@@ -1,11 +1,11 @@
 package review;
 
-public class FinallyDemo1 {
+public class FinallyDemo {
     public static void main(String[] args) {
         try {
             int a = 10 / 0;
         } catch (Exception e) {
-            System.out.println("Exception caught");
+            System.out.println("Division by Zero Exception");
         } finally {
             System.out.println("Finally block executed");
         }
